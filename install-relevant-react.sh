c34dcf08f920951760e724335ea6b16c0a6ee9a7
Glen Mailer <glenjamin@gmail.com>
2015-10-15 18:58:16 +0100

    Test on 0.14 by default

--- install-relevant-react.sh
+++ install-relevant-react.sh
@@ -1,16 +1,14 @@
 #!/bin/sh
 
-REACT=${REACT:-0.13}
+REACT=${REACT:-0.14}
 
 echo "installing React $REACT"
 
 if [ "$REACT" = "0.13" ]; then
-    npm prune
-    npm install
+    npm remove react react-dom react-addons-test-utils react-addons-create-fragment
+    npm install react
     exit
 fi
 
-npm install 'react@>0.14.0-beta.0 <=0.14'
-npm install 'react-dom@>0.14.0-beta.0 <=0.14'
-npm install 'react-addons-test-utils@>0.14.0-beta.0 <=0.14'
-npm install 'react-addons-create-fragment@>0.14.0-beta.0 <=0.14'
+npm prune
+npm install
