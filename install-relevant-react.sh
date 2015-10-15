909bb844a8e22f4e13e19d2d4b08e622569278e4
Glen Mailer <glenjamin@gmail.com>
2015-10-15 19:00:50 +0100

    Actually install 0.13

--- install-relevant-react.sh
+++ install-relevant-react.sh
@@ -6,7 +6,7 @@ echo "installing React $REACT"
 
 if [ "$REACT" = "0.13" ]; then
     npm remove react react-dom react-addons-test-utils react-addons-create-fragment
-    npm install react
+    npm install react@0.13
     exit
 fi
 
