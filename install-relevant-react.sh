f4623bb55af800a3d84ca9668c1ca46caf2431e6
jonathan schatz <jon@instamotor.com>
2017-04-10 12:36:21 -0700

    pr feedback

--- install-relevant-react.sh
+++ install-relevant-react.sh
@@ -4,8 +4,24 @@ REACT=${REACT:-0.14}
 
 echo "installing React $REACT"
 
+packages=$(cat <<-END
+  react@$REACT
+  react-dom@$REACT
+  react-addons-test-utils@$REACT
+  react-addons-create-fragment@$REACT
+  @types/react
+END
+)
+
+if [[ $REACT = "15.5" ]]; then
+  packages=$(cat <<-END
+  $packages
+  create-react-class@$REACT
+  prop-types@$REACT
+  react-test-renderer@$REACT
+END
+)
+fi
+
 npm prune
-npm install react@$REACT \
-            react-dom@$REACT \
-            react-addons-test-utils@$REACT \
-            react-addons-create-fragment@$REACT
+npm install $packages
