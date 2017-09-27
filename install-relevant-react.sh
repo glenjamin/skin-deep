271c8392e0c8394e85554c59693f8c059e3c9cc3
Philipp Spiess <hello@philippspiess.com>
2017-09-27 12:46:03 +0200

    Upgrade Tests to use React 16

--- install-relevant-react.sh
+++ install-relevant-react.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-REACT=${REACT:-15.6}
+REACT=${REACT:-16.0}
 
 echo "installing React $REACT"
 
@@ -8,12 +8,14 @@ npm prune
 
 npm install react@$REACT \
     react-dom@$REACT \
-    react-addons-test-utils@$REACT \
-    react-addons-create-fragment@$REACT \
     @types/react
 
-if node_modules/.bin/semver -r '>= 15.5' "$REACT.0"; then
-    npm install create-react-class@$REACT \
+if node_modules/.bin/semver -r '>= 15.5.0' "$REACT.0"; then
+    npm install create-react-class@15 \
         prop-types@15 \
-        react-test-renderer@15
+        react-addons-create-fragment@15 \
+        react-test-renderer@$REACT
+else
+    npm install react-addons-test-utils@$REACT \
+        react-addons-create-fragment@$REACT
 fi
